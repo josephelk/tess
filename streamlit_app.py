82e8be72103dae8fb0b86722b5e79382fcb24ee1
josephelk <162306823+josephelk@users.noreply.github.com>
2025-03-21 13:52:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+# import plotly.express as px
 
 st.title("Machine Learning App")
 st.info("This app will predict your obesity level!")
